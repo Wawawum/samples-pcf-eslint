ed1b075da7935fea5d821673a0f88e90659f1fa1
Benjamin Bertin <benjamin@wawawum.com>
2022-05-29 20:38:40 -0400

    Big Bang

---
+++ DemoESLintControl/components/List.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export class List extends React.Component {
+
+    public override render(): JSX.Element {
+
+        var uselessVar = 0;
+        return <div>Lorem ipsum dolor sit amet</div>
+    }
+}
